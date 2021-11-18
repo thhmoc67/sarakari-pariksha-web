7055b666f20c46a0de0309d8d1f5c9e1a75b6361
Dayal Sharan Saini <dayalsaini67@gmail.com>
2021-11-18 15:01:51 +0530

    basic header setup

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  bracketSpacing: false,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  arrowParens: 'avoid',
+  trailingComma: 'all',
+  tabWidth: 2,
+  semi: false
+};
---
+++ components/Layout/navbar.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Navbar() {
+  return <header>navbar</header>
+}
+
+export default Navbar
