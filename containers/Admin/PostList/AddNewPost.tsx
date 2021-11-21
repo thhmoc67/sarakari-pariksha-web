3f5aca4ad40b41fb9546bac84ffffaae231602d3
Dayal Sharan Saini <dayalsaini67@gmail.com>
2021-11-21 20:49:27 +0530

    added new post input

--- containers/Admin/PostList/AddNewPost.tsx
+++ containers/Admin/PostList/AddNewPost.tsx
@@ -3,7 +3,7 @@ import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
 import Modal from '@mui/material/Modal'
-import {IconButton} from '@mui/material'
+import {IconButton, Input, TextField} from '@mui/material'
 import {AddCircleOutlined} from '@mui/icons-material'
 
 const style = {
@@ -13,7 +13,7 @@ const style = {
   transform: 'translate(-50%, -50%)',
   width: 400,
   bgcolor: 'background.paper',
-  border: '2px solid #000',
+  border: '1px solid #aaa',
   boxShadow: 24,
   p: 4,
 }
@@ -24,7 +24,7 @@ export default function BasicModal() {
   const handleClose = () => setOpen(false)
 
   return (
-    <div className='admin-add-new-post'>
+    <div className="admin-add-new-post">
       <Button
         color="primary"
         size="large"
@@ -35,15 +35,15 @@ export default function BasicModal() {
       <Modal
         open={open}
         onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description">
+        aria-labelledby="modal-modal-title">
         <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
+          <Typography id="modal-modal-title" variant="h6" component="h1">
+            Add new post
           </Typography>
-          <Typography id="modal-modal-description" sx={{mt: 2}}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
+          <TextField fullWidth label="Post Name" color="primary" />
+          <br />
+          <br />
+          <Button fullWidth variant="contained">Add</Button>
         </Box>
       </Modal>
     </div>
