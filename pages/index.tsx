e383d44583ca2f5e32b82e041cf33ef68b8ba0ba
Dayal Sharan Saini <dayalsaini67@gmail.com>
2021-12-19 19:00:36 +0530

    fix to useeffect

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,38 +8,22 @@ import {getFirestore} from 'firebase/firestore'
 import {firebaseConfig} from '../config/firebase'
 import {collection, query, where, getDocs} from 'firebase/firestore'
 import {tagsList} from '../config/constants/tags'
+import {useEffect, useState} from 'react'
 
-interface Props {
-  results: any
-}
+interface Props {}
 
 const App: NextPage<Props> = props => {
-  return (
-    <>
-      <Head>
-        <title>Sarkari Pariksa</title>
-        <meta name="description" content="Sarkari Pariksa" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Layout>
-        <Home results={props.results} />
-      </Layout>
-    </>
-  )
-}
+  const [results, setResults] = useState({})
+  async function getAllResults() {
+    const firebaseApp = initializeApp(firebaseConfig)
+    const db = getFirestore()
+    const querySnapshot = await getDocs(collection(db, 'posts'))
 
-App.getInitialProps = async ({req}: any) => {
-  const firebaseApp = initializeApp(firebaseConfig)
-  const db = getFirestore()
-  const querySnapshot = await getDocs(collection(db, 'posts'))
-
-  const list: any = []
-  querySnapshot.forEach(doc => {
-    list.push({...doc.data(), id: doc.id})
-  })
-
-  return {
-    results: {
+    const list: any = []
+    querySnapshot.forEach(doc => {
+      list.push({...doc.data(), id: doc.id})
+    })
+    const results: any = {
       latestjobs: list
         .filter((item: any) => item.tags?.includes('latest jobs'))
         .slice(0, 9),
@@ -58,8 +42,29 @@ App.getInitialProps = async ({req}: any) => {
       admission: list
         .filter((item: any) => item.tags?.includes('admission'))
         .slice(0, 9),
-    },
+    }
+    setResults(results)
   }
+  useEffect(() => {
+    getAllResults()
+    return () => {}
+  }, [])
+  return (
+    <>
+      <Head>
+        <title>Sarkari Pariksa</title>
+        <meta name="description" content="Sarkari Pariksa" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Layout>
+        <Home results={results} />
+      </Layout>
+    </>
+  )
+}
+
+App.getInitialProps = async ({req}: any) => {
+  return {}
 }
 
 export default App
